cf7f9bbc2e55ce9008015c4e8da2dffa7b4db866
SantyMejia20 <saamejiaga@unal.edu.co>
2025-11-10 11:21:47 -0500

    Solución

--- homework/pregunta_06.py
+++ homework/pregunta_06.py
@@ -4,7 +4,7 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 `tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
 librerias de pandas para resolver las preguntas.
 """
-
+import pandas as pd
 
 def pregunta_06():
     """
@@ -15,3 +15,7 @@ def pregunta_06():
     ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 
     """
+    df = pd.read_csv('files/input/tbl1.tsv', sep='\t')
+    valores_unicos = df['c4'].unique()
+    valores_unicos_upper = [str(value).upper() for value in valores_unicos]
+    return sorted(valores_unicos_upper)
